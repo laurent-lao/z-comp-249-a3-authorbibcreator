7090e8ac9797bd4feee0d4ad785450b2d424e48a
laurentlaurent <lao.laurent@gmail.com>
2019-03-07 14:09:59 -0500

    Created a Scanner array

--- src/AuthorBibCreator.java
+++ src/AuthorBibCreator.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class AuthorBibCreator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+		// TODO Prompt for an author's name
+
+		// TODO Attempt to open all 10 input files
+		Scanner [] inputFiles = new Scanner[10];
+
+		//
     }
+
+    public static void processBibFiles(){}
 }
